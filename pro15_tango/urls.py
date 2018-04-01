3ffbee73c1717e09abc352256725ca561d50b288
MicroWriting <36100230+MicroWriting@users.noreply.github.com>
2018-04-01 22:06:35 +0900

    Update urls.py

--- pro15_tango/urls.py
+++ pro15_tango/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-   path('sort/', views.sort, name='sort')
+    path('sort/', views.sort, name='sort')
 ]
